ed561bd29df178df612d3e7c17568ad2941da574
Stephan Stylianides <s_stylianides@yahoo.com>
2018-05-31 08:37:04 -0700

    initial commit

---
+++ src/main/java/edu/stephanstylianides/advancedjava/App.java
@@ -0,0 +1,13 @@
+package edu.stephanstylianides.advancedjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
